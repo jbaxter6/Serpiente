f21454061e59498568c24e7b6232934f58faf390
jbaxter6 <jbaxter6@radford.edu>
2020-08-17 10:47:50 -0400

    nav bar needs routing

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SnakeGame from './containers/SnakeGame'
+import NavBar from './components/NavBar'
 
 const App = () => {  
   return (
     <div className="App">
+      <NavBar />
       <SnakeGame />
     </div>
   );
