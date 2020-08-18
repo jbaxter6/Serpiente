5516723cb142c16d08daf6d3121819f22b36bbf6
jbaxter6 <jbaxter6@radford.edu>
2020-08-18 15:02:33 -0400

    sign-up post works with routing

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import './App.css';
 import SnakeGame from './containers/SnakeGame'
 import NavBar from './components/NavBar'
 import Login from './components/Login'
+import Signup from './components/Signup'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 const App = () => {  
   return (
+      <BrowserRouter>
+
     <div className="App">
-      {/* {<NavBar />} */}
-      {/* {<Login />} */}
-      <SnakeGame />
+        <NavBar />
+        <Switch>
+          <Route path='/login' component={Login} />
+          <Route path='/signup' component={Signup} />
+        </Switch>
+        <SnakeGame />
     </div>
+
+      </BrowserRouter>
   );
 }
 
